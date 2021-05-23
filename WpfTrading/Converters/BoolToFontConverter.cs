259889c4eab0b2ed7efe3f35c78b962aa91ca5a8
Chi Trung Nguyen <n.chitrung@castsoftware.com>
2021-05-23 21:49:43 +0200

    add WpfTrading project

---
+++ WpfTrading/Converters/BoolToFontConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfTrading
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class BoolToFontConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? "" : "#eee";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfTrading/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+namespace WpfTrading
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+
+
+        private void btnApply_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+        }
+
+
+        private void Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Checkbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var combo = ((ComboBox)sender).SelectedValue;
+            var comboItem = ((ComboBoxItem)combo).Content;           
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
