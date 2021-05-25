0f38ca22a8425c70196d1a0dd3d23b506efef463
Chi Trung Nguyen <n.chitrung@castsoftware.com>
2021-05-25 14:07:31 +0200

    Can send message to telegram channel

--- Test/Program.cs
+++ Test/Program.cs
@@ -5,13 +5,13 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using WebSocketSharp;
-
+using Telegram.Bot;
 namespace Test
 {
     class Program
     {
-        public static decimal Price { get; set; } = 0;
-        public static decimal PriceCap { get; set; } = 0.0020m;
+        public static decimal StartPrice { get; set; } = 0;
+        public static decimal PriceCap { get; set; } = 0.001m;
         public static decimal PriceMin { get; set; } = 0;
         public static decimal PriceMax { get; set; } = 0;
         public static int Iteration { get; set; } = 0;
@@ -19,7 +19,7 @@ namespace Test
         static void Main(string[] args)
         {
             
-            var uri = "wss://stream.binance.com:9443/ws/btcusdt@trade";
+            var uri = "wss://stream.binance.com:9443/ws/dogeusdt@trade";
             WebSocket client = new WebSocket(uri);
             client.Connect();
             client.OnMessage += Ws_OnMessage;          
@@ -34,7 +34,7 @@ namespace Test
             decimal.TryParse(tradeResponse.p, out decimal price);
             if (Iteration == 0)
             {
-                Price = price;
+                StartPrice = price;
                 PriceMin = price;
                 PriceMax = price;
             }
@@ -43,14 +43,35 @@ namespace Test
             if (price < PriceMin) PriceMin = price;
             Console.WriteLine("prix Min: " + PriceMin);
             Console.WriteLine("prix Max : " + PriceMax);
-            if ((PriceMax-PriceMin)/PriceMin > PriceCap)
+            if ((StartPrice - PriceMin) / PriceMin > PriceCap)
             {
-                Console.WriteLine($"Price cap > {PriceCap} Send message to telegram");
-                var client = (WebSocket)sender;
-                client.Close();               
+                DateTime.TryParse(tradeResponse.t.ToString(), out DateTime tradeDateTime);
+                string message = $"Price baisse > {PriceCap} DogecoinUsdt price = {price} Datetime= {tradeDateTime}";
+                Console.WriteLine(message);
+                Task.Run(async () => await SendTelegramMessage(message));
+                //var client = (WebSocket)sender;
+                //client.Close();
+                PriceMin = PriceMax = StartPrice = price;
             }
-           // Thread.Sleep(500);
+            else if ((PriceMax - StartPrice) / PriceMin > PriceCap)
+            {
+                DateTime.TryParse(tradeResponse.t.ToString(), out DateTime tradeDateTime);
+                string message = $"Price monte > {PriceCap} DogecoinUsdt price = {price} Datetime= {tradeDateTime}";
+                Console.WriteLine(message);
+                Task.Run(async () => await SendTelegramMessage(message));
+                //var client = (WebSocket)sender;
+                //client.Close();
+                PriceMin = PriceMax = StartPrice = price;
+            }
+            // Thread.Sleep(500);
         }
+        private static async Task SendTelegramMessage(string message)
+        {
+            string token = "1794207254:AAGW_paSPms-Et35OdGmJLfIi6TbVVkJhBg";
+            string id = "-1001468772498";
+            var client = new TelegramBotClient(token);
+            await client.SendTextMessageAsync(id,message);
+        } 
 
  
         static void TestWcf()
